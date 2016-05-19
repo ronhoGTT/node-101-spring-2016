48c1d38f5af931df805fba68697644cc0b2e6a95
Ronnie Ho <ronho@expedia.com>
2016-05-18 22:06:24 -0700

    Week 3 Homework

---
+++ Homework/week-3/solution.js
@@ -0,0 +1,44 @@
+var myFunc = function calculate(equation){
+    var colors = require('colors');
+    var elements = equation.split(' ');
+    var result;
+    
+
+    console.log(elements);
+    
+    elements.forEach(function(val, i){
+        var operation;
+        
+        if(i === 0){
+            result = Number(val);
+        } else if (isNaN(val)){
+            switch (val){
+                case '+':
+                    result += Number(elements[i + 1]);
+                    break;
+                case '-':
+                    result -= Number(elements[i + 1]);
+                    break;
+                case '/':
+                    result /= Number(elements[i + 1]);
+                    break;
+                case 'x':
+                    result *= Number(elements[i + 1]);
+                    break;
+                case '^':
+                    result = Math.pow(result, Number(elements[i + 1]));
+                    break;
+            }
+        }
+    });
+    
+    console.log('result: ' + result);
+    if(result > 0) return colors.green(result);
+    if(result === 0) return colors.yellow(result);
+    if(result < 0) return colors.red(result);
+    
+
+    
+}
+
+module.exports = myFunc;
